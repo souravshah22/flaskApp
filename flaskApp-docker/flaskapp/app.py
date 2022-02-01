aabbdd8783053d28fc3fa7eb598bf765414317e8
sourav <souravshah22@gmail.com>
2022-02-01 16:45:30 +0530

    changing display content

--- flaskApp-docker/flaskapp/app.py
+++ flaskApp-docker/flaskapp/app.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'HELLO WORLD'
+    return 'HELLO WORLD - my name is sourav you are visiting my website'
 app.run(host="0.0.0.0", port=5000)
